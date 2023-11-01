84d2f5b0cf07dc4bd9a1565fb55ce9f4ca322551
DavisTarvids17 <davis.tarvids1@gmail.com>
2023-11-01 17:28:20 +0200

    2.uzdevums

--- 2uzd.py
+++ 2uzd.py
@@ -1,5 +1,23 @@
 """
 Iegūt ziņas, izmantojot rss no google.lv.
 
+RSS? 
 """
 
+
+import feedparser
+
+# jānorāda RSS plūsmas URL
+
+rss_url='https://news.google.com/rss?hl=lv&gl=LV&ceid=LV:lv'
+
+# lejupielādēt un izanalizēt plūsmu
+
+feed=feedparser.parse(rss_url)
+
+# parādīt ziņu virsrakstus un saites
+
+for entry in feed.entries:
+    print("Virsraksts:", entry.title)
+    print("Saite", entry.link)
+    print("\n")
