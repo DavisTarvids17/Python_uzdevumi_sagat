742ff8aadb1368b5c819f97baee5b73364d882d6
DavisTarvids17 <davis.tarvids1@gmail.com>
2023-11-01 17:38:14 +0200

    3.uzd

--- 3uzd.py
+++ 3uzd.py
@@ -4,4 +4,13 @@ Dati. Izveidot objektu, kas inicializētu atribūtus,
 piemēram, vārdu, vecumu un ceļojumam iemaksāto summu.
 
 """
+class Dati:
+    def __init__(self, vards, vecums, iemaksa_celojumam):
+        self.vards = vards
+        self.vecums = vecums
+        self.iemaksa_celojumam = iemaksa_celojumam
+
+# Izveidojam objektu ar norādītajiem atribūtiem
+mans_objekts = Dati("Doms", 29, 350)
+
 
